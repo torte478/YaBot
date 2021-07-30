066222f467251fbb000d527a694407526382db6a
Дмитрий Данилин <d.danilin@bars.group>
2021-07-30 18:58:40 +0300

    feature(*): add hello-world-app

---
+++ src/YaBot/YaBot.App/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace YaBot.App
+{
+    using System;
+    using System.IO;
+    using Telegram.Bot;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = File.ReadAllText("token");
+            var botClient = new TelegramBotClient(token);
+            
+            var me = botClient.GetMeAsync().Result;
+            
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+        }
+    }
+}
