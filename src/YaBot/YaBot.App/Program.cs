ae690c512c8b98c7867cbc223fc37284b8f15129
Дмитрий Данилин <d.danilin@bars.group>
2021-07-30 20:14:52 +0300

    feature(Program): add working polling example

--- src/YaBot/YaBot.App/Program.cs
+++ src/YaBot/YaBot.App/Program.cs
@@ -1,19 +1,51 @@
 ﻿namespace YaBot.App
 {
     using System;
-    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Telegram.Bot;
+    using Telegram.Bot.Extensions.Polling;
+    using Telegram.Bot.Types;
+    using Telegram.Bot.Types.Enums;
+    using File = System.IO.File;
 
     class Program
     {
-        static void Main(string[] args)
+        private static ITelegramBotClient botClient;
+    
+        static async Task Main(string[] args)
         {
             var token = File.ReadAllText("token");
-            var botClient = new TelegramBotClient(token);
-            
-            var me = botClient.GetMeAsync().Result;
-            
-            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+            var bot = new TelegramBotClient(token);
+
+            var me = await bot.GetMeAsync();
+            Console.WriteLine(me.Username);
+
+            var cancelation = new CancellationTokenSource();
+
+            var task =  bot.ReceiveAsync<UpdateHandler>(cancelation.Token);
+
+            Console.ReadLine();
+            cancelation.Cancel();
+        }
+        
+        private class UpdateHandler : IUpdateHandler
+        {
+            public Task HandleUpdate(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+                if (update.Message == null)
+                    return Task.CompletedTask;
+                
+                return botClient.SendTextMessageAsync(update.Message.Chat, $"Answer: {update.Message.Text}");
+            }
+
+            public Task HandleError(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            {
+                return botClient.SendTextMessageAsync(123, exception.ToString());
+            }
+
+            public UpdateType[]? AllowedUpdates => new[] { UpdateType.Message };
         }
     }
 }
