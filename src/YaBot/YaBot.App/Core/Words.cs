9ae5c803fe6ff9eec4fcd855e759309453aad02c
Дмитрий Данилин <d.danilin@bars-open.ru>
2021-09-17 19:52:06 +0300

    bug(Words): fix char lowercase

--- src/YaBot/YaBot.App/Core/Words.cs
+++ src/YaBot/YaBot.App/Core/Words.cs
@@ -4,35 +4,49 @@
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Linq;
+    using YaBot.Core.Extensions;
 
     public sealed class Words : IWords
     {
-        private readonly ImmutableArray<string> words;
+        private readonly ImmutableHashSet<string> keys;
 
         public static IWords Create(IEnumerable<string> words)
         {
-            return new Words(words.ToImmutableArray());
+            return new Words(words.ToImmutableHashSet());
         }
         
-        public Words(ImmutableArray<string> words)
+        private Words(ImmutableHashSet<string> keys)
         {
-            this.words = words;
+            this.keys = keys;
         }
 
         public bool Match(string text)
         {
-            return text != null 
-                   && words.Any(text.Contains);
+            return text != null
+                   && text._(Parse).Any(keys.Contains);
         }
         
         public IEnumerator<string> GetEnumerator()
         {
-            return words.AsEnumerable().GetEnumerator();
+            return keys.AsEnumerable().GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        private IEnumerable<string> Parse(string text)
+        {
+            var punctuation = text
+                .Where(char.IsPunctuation)
+                .Distinct()
+                .ToArray();
+
+            return text
+                .ToLower()
+                .Split()
+                .Select(_ => _.Trim(punctuation));
         }
     }
 }
---
+++ src/YaBot/YaBot.Tests/Words_Should.cs
@@ -0,0 +1,17 @@
+﻿namespace YaBot.Tests
+{
+    using NUnit.Framework;
+    using YaBot.App.Core;
+
+    [TestFixture]
+    internal sealed class Words_Should
+    {
+        [Test]
+        public void ConvertStringToLowerCase_BeforeParse()
+        {
+            var words = Words.Create(new[] { "test" });
+
+            Assert.That(words.Match("TEST"), Is.True);
+        }
+    }
+}
